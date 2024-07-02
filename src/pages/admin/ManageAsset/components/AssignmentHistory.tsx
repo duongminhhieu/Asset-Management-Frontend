19fc3c3156f288f78a3713f83c229e005a0f3f8c
Hoang Do Nhat <dnhoang214@gmail.com>
2024-07-02 04:02:54 +0000

    Merged PR 96: Fix table front end
    ![image.png](https://dev.azure.com/hoangnd-nt/f651a6e4-846e-4083-9c5e-1a6a21d36674/_apis/git/repositories/917acca9-de99-44de-9f58-f5071605802c/pullRequests/96/attachments/image.png)
    
    Related work items: #504

--- src/pages/admin/ManageAsset/components/AssignmentHistory.tsx
+++ src/pages/admin/ManageAsset/components/AssignmentHistory.tsx
@@ -1,5 +1,5 @@
 import { AssignmentHistoryDto } from "@/types/Assignment";
-import { Table } from "antd";
+import { Table, Typography } from "antd";
 
 interface Props {
   data: AssignmentHistoryDto[];
@@ -27,22 +27,46 @@ function AssignmentHistory({ data, isLoading }: Props) {
       key: "assignTo",
       title: "Assigned to",
       dataIndex: "assignTo",
-      render: (username: string) => username,
+      render: (username: string) => (
+        <Typography.Paragraph
+          ellipsis={{
+            expandable: "collapsible",
+          }}
+        >
+          {username}
+        </Typography.Paragraph>
+      ),
     },
     {
-        key: "assignBy",
-        title: "Assigned by",
-        dataIndex: "assignBy",
-        render: (username: string) => username,
-      },
+      key: "assignBy",
+      title: "Assigned by",
+      dataIndex: "assignBy",
+      render: (username: string) => (
+        <Typography.Paragraph
+          ellipsis={{
+            expandable: "collapsible",
+          }}
+        >
+          {username}
+        </Typography.Paragraph>
+      ),
+    },
     {
       key: "returnDate",
       title: "Returned Date",
       dataIndex: "returnDate",
-      render: (date: Date) => date?formatDate(date):null,
+      render: (date: Date) => (date ? formatDate(date) : null),
     },
   ];
-  return <Table columns={columns} dataSource={data} pagination={false} loading={isLoading} rowKey={(record)=>record.id} />;
+  return (
+    <Table
+      columns={columns}
+      dataSource={data}
+      pagination={false}
+      loading={isLoading}
+      rowKey={(record) => record.id}
+    />
+  );
 }
 
 export default AssignmentHistory;
